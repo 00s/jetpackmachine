1c09ceb79eb7524fde2c2d992b4623fe1d542dfc
Lucas Farias <adf.melo@gmail.com>
2014-10-23 21:47:34 -0400

    reels repositioning, add mask and readjust spinning logic

--- js/game.js
+++ js/game.js
@@ -9,41 +9,41 @@ var displayMessage = "PULL THE LEVER";
 /* constants */
 var TOKEN_VALUE = 50;
 var NUMBER_OF_REELS = 3;
+var EXTRA_SIMBOLS = -175;
+var REEL_PADDING = 50;
 
 /* enumeration for SYMBOL POSITIONS (freeze option prevent keys to be modified)*/
-var POSITION = Object.freeze({	COIN_BONUS: 	0, 
-								COIN_JACKPOT:	-99, 
-								BIG_BLAST: 		-200, 
-								DOUBLE_TOKENS: 	-300,
-								DOUBLE_COINS: 	-395, 
-								COIN_PRIZE: 	-485, 
-								SMALL_BLAST: 	-585, 
-								ATOM_BLAST: 	-685, 
-								EXTRA_SPINS: 	-786, 
-								HEAD_START: 	-890});
+var POSITION = Object.freeze({	COIN_BONUS:     -970+REEL_PADDING, 
+								COIN_JACKPOT:	-99 +REEL_PADDING, 
+								BIG_BLAST: 		-200+REEL_PADDING, 
+								DOUBLE_TOKENS: 	-300+REEL_PADDING,
+								DOUBLE_COINS: 	-395+REEL_PADDING, 
+								COIN_PRIZE: 	-485+REEL_PADDING, 
+								SMALL_BLAST: 	-585+REEL_PADDING, 
+								ATOM_BLAST: 	-685+REEL_PADDING, 
+								EXTRA_SPINS: 	-786+REEL_PADDING, 
+								HEAD_START: 	-890+REEL_PADDING});
 
 // acceleration references for the reels
 var acc = 0.2;
 // speed ref for the reels (number of pixels)
 var reels = [];
+var masks = []
 var circle;
 function init(){ 
 	// define canvas as stage
 	stage  = new createjs.Stage("slotmachine");
 
 	initializeReels(reels, NUMBER_OF_REELS);
-	// add initilized reels to stage
-	reels.forEach(function (entry){
-		stage.addChild(entry);
-	});
 
-	// set frame Ticker frame rate
 	createjs.Ticker.setFPS(60);
 	createjs.Ticker.addEventListener("tick", tick);
 
+
     //Create a Shape
     circle = new createjs.Shape();
-    circle.graphics.beginFill("red").drawCircle(0, 0, 40);
+    circle.graphics.beginRadialGradientFill(["rgba(255,255,255,1)", "rgba(205,0,0,1)"], [0, 1], -18, -18, 0, 0, 0, 35).drawCircle(10, 10, 25);
+    //circle.graphics.beginFill("red").drawCircle(0, 0, 20);
     //Set position of Shape instance.
     circle.x = 4*70;
     circle.y = 40;
@@ -66,14 +66,13 @@ function tick(event){
 }
 
 /* spin given reel:
-	condition - relativeDistance (from nextStop) must be greater than 0	
- */
+	condition - relativeDistance (from nextStop) must be greater than 0	 */
 function spinReel(reel){
 	if(reel.relativeDistance > 0){
 		reel.spinning = true;
 		//return the image to start position after last symbol is shown
-		if(reel.y >= 0){
-			reel.y -= reel.image.height;
+		if(reel.y >= POSITION.COIN_JACKPOT){
+			reel.y = POSITION.HEAD_START + EXTRA_SIMBOLS;
 		}
 		// set max speed
 		if(reel.relativeDistance <= 500){
@@ -98,7 +97,7 @@ function spinReel(reel){
 
 // auxiliar funtion returns distance (in pixels) based on number of complete turns
 function completeTurn(num){
-	return reels[0].image.height*num;
+	return (970)*num;
 }
 
 function setNextStop(){
@@ -125,14 +124,25 @@ function reelsStopped(){
 
 // fill up the array with the reels and set up variables
 function initializeReels(reelsArray, numberOfReels){
+
+	var xOffset = 85;
 	for (var i = 0; i <numberOfReels; i++){
-		reelsArray.push(new createjs.Bitmap("assets/img/reel.jpeg"));
+		reelsArray.push(new createjs.Bitmap("assets/img/reelz.jpeg"));
 		reelsArray[i].relativeDistance = 0; // reference for reel next position
-		reelsArray[i].y = POSITION.COIN_JACKPOT; // start position
-		reelsArray[i].x = i*80; // space based on img width
+		reelsArray[i].x = i*xOffset+1; // space based on img width
 		reelsArray[i].speed = 0.5; //start speed
-		reelsArray[i].nextStop = POSITION.COIN_JACKPOT;
+		reelsArray[i].nextStop = POSITION.COIN_BONUS;
 		reelsArray[i].spinning = false;
+
+		//mask for reels
+		var rectMask = new createjs.Shape();
+		rectMask.graphics.beginStroke('#fe0') .setStrokeStyle(2).rect(xOffset*i+1, 0, 80, 170);
+		// add mask to array, set mask to reel and stage mask
+		masks.push(rectMask);
+		reelsArray[i].mask =rectMask;
+		//stage the elements
+		stage.addChild(reelsArray[i]);
+		stage.addChild(rectMask);
 	}
 }
 
